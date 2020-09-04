32c011134c2031cdb9455270d1aaea20f5740259
Mr Rajput <daksharajkamal111@gmail.com>
2020-09-04 17:45:52 +0530

    Screens

---
+++ screens/LoadingScreen.js
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+import { View, StyleSheet, ActivityIndicator } from "react-native";
+import * as firebase from "firebase";
+class LoadingScreen extends Component {
+  componentDidMount() {
+    this.checkIfLoggedIn();
+  }
+  checkIfLoggedIn = () => {
+    firebase.auth().onAuthStateChanged(
+      function (user) {
+        if (user) {
+          this.props.navigation.navigate("DashboardScreen");
+        } else {
+          this.props.navigation.navigate("LoginScreen");
+        }
+      }.bind(this)
+    );
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <ActivityIndicator size="large"></ActivityIndicator>
+      </View>
+    );
+  }
+}
+export default LoadingScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
